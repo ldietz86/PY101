8b742e6b3611d434b6a8f83d826e028e7b6f1e27
Lakpa Dietz <ldietz086@gmail.com>
2026-07-31 08:21:34 -0700

    Added helper functions and input validation

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -1,33 +1,55 @@
 '''
 Command-line Calculator
 '''
-
 # Ask the user for the first number.
 # Ask the user for the second number.
 # Ask the user for an operation to perform.
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
 
-print('Welcome to Calculator!')
+def prompt(message):
+    print(f"==> {message}")
 
-print("What's the first number?")
-number1 = int(input())
+def invalid_number(number_str):
+    try:
+        int(number_str)
+    except ValueError:
+        return True
 
-print("What's the second number?")
-number2 = int(input())
+    return False
 
-print(
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number?")
+number1 = input()
+
+while invalid_number(number1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number1 = input()
+
+prompt("What's the second number?")
+number2 = input()
+
+while invalid_number(number2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    number2 = input()
+
+prompt(
     'What operation would you like to perform?\n'
     '1) Add 2) Subtract 3) Multiply 4) Divide')
 operation = input()
 
-if operation == '1':
-    print(f'The result is: {number1 + number2}')
-elif operation == '2':
-    print(f'The result is: {number1 - number2}')
-elif operation == '3':
-    print(f'The result is: {number1 * number2}')
-elif operation == '4':
-    print(f'The result is: {number1 / number2}')
-else:
-    print('Please enter a number from 1 to 4')
+while operation not in ["1", "2", "3", "4"]:
+    prompt('You must choose 1, 2, 3, or 4')
+    operation = input()
+
+match operation:
+    case '1':
+        prompt(f'The result is: {int(number1)+ int(number2)}')
+    case '2':
+        prompt(f'The result is: {int(number1) - int(number2)}')
+    case '3':
+        prompt(f'The result is: {int(number1)* int(number2)}')
+    case '4':
+        prompt(f'The result is: {int(number1) / int(number2)}')
+
